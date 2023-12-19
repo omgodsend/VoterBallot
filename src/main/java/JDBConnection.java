682525a030292a247026ac564cad0301a804e920
Tarek <covermytrack@gmail.com>
2023-12-19 14:27:30 -0500

    jdb connection

--- src/main/java/JDBConnection.java
+++ src/main/java/JDBConnection.java
@@ -1,4 +1,24 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class JDBConnection {
 
-    public static final String DB_URL = "jdbc:mysql://localhost:3306/Voterdb";
+    public static final String DB_URL = "jdbc:mysql://localhost:3306/voterdb";
+
+    private static final String DB_USER = "root";
+
+    private static final String DB_PASSWORD = "";
+
+    public static Connection getConnection(){
+        Connection conn = null;
+
+        try {
+            conn = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
+            System.out.println("Connected to db successfully");
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return conn;
+    }
 }
